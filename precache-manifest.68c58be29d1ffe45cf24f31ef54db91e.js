76f66f668a82386b7b581bb59b37f6b5eca2a056
Irek Iwanca <iiwanca@gmail.com>
2020-11-02 12:48:56 +0100

    Updates

---
+++ precache-manifest.68c58be29d1ffe45cf24f31ef54db91e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "684b367bcbdb51563610",
+    "url": "/weather-app/static/css/main.1ef78618.chunk.css"
+  },
+  {
+    "revision": "684b367bcbdb51563610",
+    "url": "/weather-app/static/js/main.9c19fe96.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/weather-app/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "2571e0b33070ebd024f69705a8d9593a",
+    "url": "/weather-app/index.html"
+  }
+];
